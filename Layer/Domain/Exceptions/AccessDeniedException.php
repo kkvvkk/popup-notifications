8411390e85f47e9be319b6c48ffdda65b3f3427f
vk <v.koshcheev@raketa.travel>
2023-11-15 18:15:51 +0500

    add Domain exceptions

---
+++ Layer/Domain/Exceptions/AccessDeniedException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Layer\Domain\Exceptions;
+
+use Exception;
+
+class AccessDeniedException extends Exception
+{
+}
---
+++ Layer/Domain/Exceptions/NotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Layer\Domain\Exceptions;
+
+use Exception;
+
+class NotFoundException extends Exception
+{
+}
